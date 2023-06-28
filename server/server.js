e65311e960adb2a9693ba0d3b2dc2ef37b68aa23
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-06-28 17:01:32 +0530

    url changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { SocketServer } = require("./socket");
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://rrp-amb.netlify.app/",
   },
 });
 
