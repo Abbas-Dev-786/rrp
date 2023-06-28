1cbc763ac55fe660bd3d2e7742c2a2981bdc95e4
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-06-28 17:09:16 +0530

    updated cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors({ origin: "https://rrp-amb.netlify.app/" }));
 
 app.get("/", (req, res) => {
   res.status(200).end();
