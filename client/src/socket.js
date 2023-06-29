452c4036e43e3941ebf9250474c7b4190e5bbbd3
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-06-29 09:08:52 +0530

    fixed env variables

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,12 @@
 import { io } from "socket.io-client";
 
-// const URL = "https://rrp-backend.onrender.com";
-const URL = "http://localhost:8000";
+// const URL =
+const URL =
+  import.meta.env.REACT_APP_ENV === "pro"
+    ? "https://rrp-backend.onrender.com"
+    : "http://localhost:8000";
+
+console.log(import.meta.env);
 
 export const socket = io(URL, {
   autoConnect: false,
