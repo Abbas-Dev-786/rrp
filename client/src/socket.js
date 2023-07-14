ac808b1e8de9d8209cc2c6b0504b75cd47a6003c
Abbas Bhanpura wala <99265597+Abbas-Dev-786@users.noreply.github.com>
2023-07-14 08:32:09 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // const URL =
 const URL = import.meta.env.PROD
-  ? "https://rrp-backend.onrender.com"
+  ? "rrp-amb.up.railway.app"
   : "http://localhost:8000";
 
 export const socket = io(URL, {
